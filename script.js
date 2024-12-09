42917de1d5c355fe8dec1199bfc9f885637d447e
MJ lynch <mjlynch35@yahoo.com>
2024-12-09 11:20:42 -0600

    Added basic digital clock to screen

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function updateClock() {
-    const clockElement = document.getElementById('clock');
+    const clockElement = document.querySelector('.clock');
     const now = new Date();
 
+    // Getting hours, minutes and seconds
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
@@ -10,4 +11,12 @@ function updateClock() {
     hours = hours < 10 ? `0${hours}` : hours;
     minutes = minutes < 10 ? `0${minutes}` : minutes;
     seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+
+    clockElement.textContent = currentTime;
 }
+
+setInterval(updateClock, 1000);
+
+updateClock();
