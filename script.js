9c95ea5a41ccd4e1aee5463f185e6ea728416b49
MJ lynch <mjlynch35@yahoo.com>
2024-12-09 11:18:22 -0600

    Adding leading zeroes if needed

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ function updateClock() {
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
+
+    // Adding leading zeros
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
 }
