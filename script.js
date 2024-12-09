a1144c69e9307bfe2a8db4bcb324063a40071be7
MJ lynch <mjlynch35@yahoo.com>
2024-12-09 11:15:45 -0600

    Adding variables to JS file

---
+++ script.js
@@ -0,0 +1,8 @@
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+}
